9de3f46362f06bd44d0deb6b2c9f0ef1b0f9f734
JPLABRADOR <vanhagarsux@gmail.com>
2020-06-17 18:22:07 -0700

    Lesson 13 Project Data Files

---
+++ testAssets/homeLoanWizardInvalidArray.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        purchasePrice: "aaaaaa",
+        downPayment: "aaaaaa",
+        overviewPrice: "aaaaaa",
+        overviewDownPayment: "aaaaaa"
+    },
+    {
+        purchasePrice: "//////",
+        downPayment: "//////",
+        overviewPrice: "//////",
+        overviewDownPayment: "//////"
+    }
+]
